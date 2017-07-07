7de7fdd6e961db6425f0533a7fff7b0b3a815e53
jhelson15 <jelsonbbarrozo@gmail.com>
2017-07-07 23:08:54 +0800

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -3,10 +3,10 @@
 
 read -p "Username : " Login
 read -p "Password : " Pass
-read -p "Expired (hari): " masaaktif
+read -p "Expired (Date): " Date
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+useradd -e `date -d "$Date days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
@@ -19,7 +19,7 @@ echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
-echo -e "Aktif Sampai: $exp"
+echo -e "Expired Date: $exp"
 echo -e "============================="
-echo -e "Mod by Abu Yazid Al-Busthami"
+echo -e "Mod by jhelson15"
 echo -e ""
