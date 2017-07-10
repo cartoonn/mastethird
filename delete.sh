313f5238929c28d5bff14dc9c4090c13cd6b79e9
jhelson15 <jelsonbbarrozo@gmail.com>
2017-07-10 18:55:47 +0800

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -2,14 +2,13 @@
 # Script Delete user account SSH
 # Created by jhelson15
 
-3
-
+4
 read -p "Type Username account to Delete : " Nama
 
 userdel -r $Nama 
 
 clear
 
-echo $Name "Account successfully deleted!"
+echo "Account successfully deleted!"
 echo "Type (menu) to back option!."
 
