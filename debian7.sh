c3096e697380a5802d77ecd2b7b72e58481cf164
jhelson15 <jelsonbbarrozo@gmail.com>
2017-07-12 00:35:03 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -82,7 +82,11 @@ wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/rizal180499/Au
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+sysctl -w net.ipv4.conf.icmp_echo_ignore_all = 1
+sed -i 's/#nnet.ipv4.conf.icmp_echo_ignore_all = 1/nnet.ipv4.conf.icmp_echo_ignore_all = 1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 10.8.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/16 -o venet0 -j SNAT --to-source `curl ipecho.net/plain
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables-save > /etc/iptables_yg_baru_dibikin.conf
 wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
