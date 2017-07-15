0535c8f9db6d402823b90a8d8c4d8ae27ef72839
Jbbarrozo <jelsonbbarrozo@gmail.com>
2017-07-16 01:16:10 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -29,10 +29,17 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
 # set repo
-wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/sources.list.debian7"
-wget "http://www.dotdeb.org/dotdeb.gpg"
-wget "http://www.webmin.com/jcameron-key.asc"
-cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+#wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/sources.list.debian7"
+#wget "http://www.dotdeb.org/dotdeb.gpg"
+#wget "http://www.webmin.com/jcameron-key.asc"
+#cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cd /root
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+apt-get update
+apt-get -y install webmin
 cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
 # update
