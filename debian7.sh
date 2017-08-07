ead07fa00fdeea1078ad9be9aeffe1367966c854
Jelson barrozo <jelsonbbarrozo@gmail.com>
2017-08-08 01:41:08 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -82,7 +82,7 @@ wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/jhelson15/
 service nginx restart
 
 # install openvpn
-###wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
 wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/rizal180499/Auto-Installer-VPS/master/conf/1194.conf"
@@ -90,15 +90,15 @@ service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
-###iptables-save > /etc/iptables_yg_baru_dibikin.conf
-###wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/iptables"
+iptables-save > /etc/iptables_yg_baru_dibikin.conf
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
 
 #konfigurasi openvpn
 cd /etc/openvpn/
-###wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/client-1194.conf"
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/client-1194.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
 cp client.ovpn /home/vps/public_html/
 
